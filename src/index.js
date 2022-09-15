582688f3c8146bdcdba1e1cda8e4c151dbfdaccf
Programmer-Mehadi <mehadi.developer@gmail.com>
2022-09-15 23:40:00 +0600

    react Fragment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 // import reportWebVitals from './reportWebVitals';
 
 /* first parameter ki dakhabo tai , 2nd parameter kothay dekhabo. */
@@ -15,13 +16,19 @@ way-2:
 <div ><h1>Muhammad Mehadi</h1>
 <h3>I am Web Developer.</h3></div>
 */
-ReactDOM.render(<div ><h1>Muhammad Mehadi</h1>
-<h3>I am Web Developer.</h3></div>
-  , document.getElementById('root'));
+/*
+React fragment: don't need create any other div/a/h1/any tag.
+way-1: <React.Fragment></React.Fragment>
+way-2:<></>
+*/
 
-
-
-
+ReactDOM.render(
+  <React.Fragment>
+    <h1>Muhammad Mehadi</h1>
+    <h3>I am Web Developer.</h3>
+  </React.Fragment>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
